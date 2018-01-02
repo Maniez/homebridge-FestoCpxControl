2db6a270a964a5de922f32fab90e4323d12aaf85
Maniez <maniez@gmx,de>
2018-01-02 12:58:36 +0100

    Removed debug code

--- udp.js
+++ udp.js
@@ -6,9 +6,7 @@ module.exports = function (host, port, payload, callback) {
   setTimeout(function() {
       client.send(message, 0, message.length, port, host, function(err, bytes) {
         if (err) throw err;
-        console.log('UDP message sent to ' + host +':'+ port);
         client.close();
-
         callback(err);
       });
   }, 50);
