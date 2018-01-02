011aae4a0a229b400ff70bd96478c340eb78756d
Maniez <maniez@gmx,de>
2018-01-02 12:57:12 +0100

    Removed old Code

--- udp.js
+++ udp.js
@@ -1,17 +1,9 @@
 module.exports = function (host, port, payload, callback) {
-  var broadcast = '000000000000000009000000e00729070b00170a00000000c0a80a0555c100008ec20000000006000000000000000000'
-
   var message = new Buffer(payload, 'hex');
-
   var dgram = require('dgram');
   var client = dgram.createSocket('udp4');
 
-
-  setTimeout(function() { 
-    //client.send(broadcast, 0, broadcast.length, port, host, function(err, bytes) {
-    //  if (err) throw err;
-    //  console.log('UDP message sent to ' + host +':'+ port);
-
+  setTimeout(function() {
       client.send(message, 0, message.length, port, host, function(err, bytes) {
         if (err) throw err;
         console.log('UDP message sent to ' + host +':'+ port);
@@ -19,7 +11,5 @@ module.exports = function (host, port, payload, callback) {
 
         callback(err);
       });
-
-   // });
   }, 50);
 }
